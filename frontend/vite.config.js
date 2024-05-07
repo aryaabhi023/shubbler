be0b606e9466fb06f68e923be0d2e5af21ae033f
aryaabhi023 <abhishekparmar2306@gmail.com>
2024-05-07 17:16:43 +0530

    add changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:7004",
+      "/api": "https://shubbler-api.onrender.com/",
     },
   },
 });
