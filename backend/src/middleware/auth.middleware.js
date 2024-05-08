a6c7393576269c1eedf8ef6852375b4f28abc88b
aryaabhi023 <abhishekparmar2306@gmail.com>
2024-05-08 15:12:06 +0530

    add changes

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -9,7 +9,7 @@ export const verifyJWT = async function (req, res, next) {
     if (!token || token === "undefined") {
       return res.status(401).json("Unauthorized request");
     }
-
+    console.log(token, "Hey");
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decodedToken?._id).select(
       "-password -refeshToken"
